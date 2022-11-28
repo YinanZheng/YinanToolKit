667312000ef65943f9f3ecd37976d079217a88f2
Yinan Zheng <y-zheng@northwestern.edu>
2022-11-27 20:45:44 -0600

    Update BioinformaticAnalysis.R
    Manhattan Plot and Gene Enrichment

--- BioinformaticAnalysis.R
+++ BioinformaticAnalysis.R
@@ -1,45 +1,118 @@
-# Standard Enrichment analysis
+# Manhattan plot
 
-### Install package if necessary
+library(CMplot)
 
+## Flat
+
+## res_CC_plot is a dataframe with 4 columns: CpG ID, Chromosome, Position, p-value. (The column names does not matter) 
+
+CMplot(res_CC_plot, plot.type="m", col=c("grey30"), LOG10=TRUE, threshold=c(5.965213e-08),
+       threshold.lty=c(1), threshold.lwd=c(1), threshold.col=c("black"), amplify=TRUE,
+       chr.den.col=NULL, signal.col=c("red"), cex = 0.8, signal.cex=c(0.8),signal.pch=c(19),
+       file="jpg",memo="",dpi=600,file.output=TRUE,verbose=TRUE,width=14,height=6,chr.labels.angle=45)
+
+## Note: Use those "highlight" parameter family to mark out the significant results. (Use CpG ID to specify which CpG to 
+## highlight)
+
+
+###################################################################################################################
+
+
+# Enrichment plot
+
+library(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
 library(clusterProfiler)
-library(DOSE)
-library(AnnotationDbi)
+library(missMethyl)
 library(org.Hs.eg.db)
+library(enrichplot)
+library(ggplot2)
+library(DOSE)
+library(ReactomePA)
+
+annot <- getAnnotation(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
+
+####
+
+# cpg_list is a character vector of significant CpG IDs.
+
+EntrezID<- getMappedEntrezIDs(sig.cpg = cpg_list, array.type = "EPIC")
+
+## GO (BP)
+ego <- enrichGO(gene          = EntrezID$sig.eg,
+                OrgDb         = org.Hs.eg.db,
+                ont           = "BP",
+                pAdjustMethod = "BH",
+                pvalueCutoff  = 1,
+                qvalueCutoff  = 0.2,
+                readable      = TRUE)
+data.frame(ego)
+ego2 <- pairwise_termsim(ego)
+p1 <- treeplot(ego2,nCluster = 3, method = "Wang")
+p1
+
+write.xlsx(data.frame(ego), file="GeneEnrichment_qval0.2.xlsx",
+           sheetName="GO-BP", append=FALSE)
+
+## GO (MF)
+ego <- enrichGO(gene          = EntrezID$sig.eg,
+                OrgDb         = org.Hs.eg.db,
+                ont           = "MF",
+                pAdjustMethod = "BH",
+                pvalueCutoff  = 1,
+                qvalueCutoff  = 0.2,
+                readable      = TRUE)
+data.frame(ego)
+ego2 <- pairwise_termsim(ego)
+p1 <- treeplot(ego2,nCluster = 3, method = "Wang")
+p1
+
+write.xlsx(data.frame(ego), file="GeneEnrichment_qval0.2.xlsx",
+           sheetName="GO-MF", append=TRUE)
 
 
-### The input is the RefGene ID (NM_xxxxx or NR_xxxxx) from the Illumina Annotation file (i.e. the "UCSC_RefGene_Accession" column)
-
-UCSC_RefGene_Accession <- NA
-
-### 
-
-## Split the ID and make a unique set
-REGSEQ <- unique(unlist(strsplit(as.character(UCSC_RefGene_Accession), ";")))
-
-## Translate ID to Entrez ID
-geneSymbol <- select( org.Hs.eg.db, keys = REGSEQ,
-                      columns = "ENTREZID",  keytype = "REFSEQ" )
-
-
-
-### KEGG enrichment anlaysis
-
-kk <- enrichKEGG(gene         = geneSymbol$ENTREZID,
+## KEGG
+kk <- enrichKEGG(gene         = EntrezID$sig.eg,
                  organism     = 'hsa',
-                 pvalueCutoff = 0.05)
+                 pAdjustMethod = "BH",
+                 pvalueCutoff  = 1,
+                 qvalueCutoff  = 0.2)
 head(kk)
 
-## save the results to current folder
-write.csv(kk, "./KEGG_enrich.csv")
+kkx <- setReadable(kk, 'org.Hs.eg.db', 'ENTREZID')
+p1 <- cnetplot(kkx, showCategory = 10, cex_label_gene = 0.5)
+p1
+
+write.xlsx(data.frame(kk), file="GeneEnrichment_qval0.2.xlsx",
+           sheetName="KEGG", append=TRUE)
 
 
-#### GO enrichment anlaysis
-ego <- enrichGO(gene         = geneSymbol$ENTREZID,
-                OrgDb = 'org.Hs.eg.db', ont = "BP",
-                pvalueCutoff = 0.05)
-head(ego)
+## Reactome
+x <- enrichPathway(gene=EntrezID$sig.eg,               
+                   pAdjustMethod = "BH",
+                   pvalueCutoff  = 1,
+                   qvalueCutoff  = 0.2,
+                   readable      = TRUE)
+head(x)
+edox <- setReadable(x, 'org.Hs.eg.db', 'ENTREZID')
 
-## save the results to current folder
-write.csv(ego, "./GO_enrich.csv")
+p1 <- cnetplot(edox,showCategory = 10, cex_label_gene = 0.5)
+p1
 
+write.xlsx(data.frame(edox), file="GeneEnrichment_qval0.2.xlsx",
+           sheetName="Reactome", append=TRUE)
+
+
+## Disease gene network
+edo <- enrichDGN(EntrezID$sig.eg,
+                 pAdjustMethod = "BH",
+                 pvalueCutoff  = 1,
+                 qvalueCutoff  = 0.2,
+                 readable      = TRUE)
+
+edox <- setReadable(edo, 'org.Hs.eg.db', 'ENTREZID')
+
+p1 <- cnetplot(edox,showCategory = 10, cex_label_gene = 0.5)
+p1
+
+write.xlsx(data.frame(edox), file="GeneEnrichment_qval0.2.xlsx",
+           sheetName="DGN", append=TRUE)
